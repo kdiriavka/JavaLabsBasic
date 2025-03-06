60191e2e3c7b85fd6425675ee92571226534e000
Kostia <kosdi4321@gmail.com>
2025-03-06 20:53:20 +0200

    Lab1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hello World!");
-        System.out.print("Hello World!");
     }
 }
