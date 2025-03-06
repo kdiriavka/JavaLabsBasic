8aa33109bcc56ecdd92a4aa9e6cf62fc620f7132
Kostia <kosdi4321@gmail.com>
2025-03-06 14:13:38 +0200

    Lab3

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -10,23 +10,23 @@ public class Run {
     }
 
     public static void createTypicalUniversity() {
-        Human rector = new Human("Іван", "Петров", "Олександрович", Sex.MALE) {};
+        Human rector = new Human("Іван", "Петров", "Олександрович", Gender.MALE) {};
 
         // Створюємо студентів
         List<Student> students = new ArrayList<>();
-        students.add(StudentCreator.createStudent("Олексій", "Іванов", "Сергійович", Sex.MALE));
-        students.add(StudentCreator.createStudent("Марія", "Петренко", "Володимирівна", Sex.FEMALE));
+        students.add(StudentCreator.createStudent("Олексій", "Іванов", "Сергійович", Gender.MALE));
+        students.add(StudentCreator.createStudent("Марія", "Петренко", "Володимирівна", Gender.FEMALE));
 
         // Створюємо групу
         Human groupHead = students.get(0);
         Group group = GroupCreator.createGroup("ІП-22", groupHead, students);
 
         // Створюємо кафедру
-        Human departmentHead = new Human("Олег", "Сидоренко", "Петрович", Sex.MALE) {};
+        Human departmentHead = new Human("Олег", "Сидоренко", "Петрович", Gender.MALE) {};
         Department department = DepartmentCreator.createDepartment("Кафедра ІТ", departmentHead, List.of(group));
 
         // Створюємо факультет
-        Human facultyHead = new Human("Андрій", "Коваленко", "Ігорович", Sex.MALE) {};
+        Human facultyHead = new Human("Андрій", "Коваленко", "Ігорович", Gender.MALE) {};
         Faculty faculty = FacultyCreator.createFaculty("Факультет Комп'ютерних Наук", facultyHead, List.of(department));
 
         // Створюємо університет
